2701da6e6363cdadafd1a3c49726edce84ac2d82
Kang Seunghwan <rnltls97@gmail.com>
2020-04-20 21:07:38 +0900

    added card drawing

--- game.py
+++ game.py
@@ -40,6 +40,16 @@ class Button():
             if self.posn.y < pos[1] <self.posn.y + self.height:
                 return True
         return False
+
+class Card():
+    def __init__(self, card_tuple, x, y):
+        self.x = x
+        self.y = y
+        self.width = win_size[0]*(3/20)
+        self.height = win_size[1]*((17*72)/3000)
+        self.card = card_tuple
+    pass
+
 class Table():
     def __init__(self, color, win_size):
         self.color = color
@@ -47,12 +57,25 @@ class Table():
         self.y = win_size[1]/2 - win_size[1]/15
         self.width = win_size[0]
         self.height = win_size[1]-self.y
+        self.card_width = self.width*(3/20)
+        self.card_height = self.height*(72/100)
+        self.card_hand = []
     
     def draw(self, window, boarder = BROWN):
         pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height), 0)
         pygame.draw.rect(window, boarder, (self.x, self.y, self.width, self.height/10), 0)
-    def put_on_card(self):
-        pass
+    
+    def put_on_card(self, card_tuple):
+        idx = len(self.card_hand)
+        x_pos = self.x + (1 + idx)*self.width*(1/24) + len(self.card_hand)*self.card_width
+        y_pos = (self.y+self.height/10) + self.height*(9/100)
+        self.card_hand.append(Card(card_tuple, x_pos, y_pos))
+        
+    def draw_card(self, window, idx):
+        pygame.draw.rect(window, WHITE, (self.card_hand[idx].x, self.card_hand[idx].y, self.card_hand[idx].width, self.card_hand[idx].height), 0)
+        font = pygame.font.SysFont('comicsans', 60)
+        text = font.render(str(self.card_hand[idx].card), 1, (0,0,0))
+        window.blit(text, (self.card_hand[idx].x + (self.card_hand[idx].width/2 - text.get_width()/2), self.card_hand[idx].y + (self.card_hand[idx].width/2 - text.get_height()/2)))
 startButton = Button(LIGHTBLUE, pnr.Point(515, 630), 250, 100, 'Start!')
 pokertable = Table(TABLE, win_size)
 ready = True
@@ -76,6 +99,10 @@ while run:
     pokertable.draw(screen)
     if ready:
         startButton.draw(screen, BROWN)
+    else:
+        pokertable.put_on_card(('H','A'))
+        pokertable.draw_card(screen,0)
+
     # This MUST happen after all the other drawing commands
     pygame.display.update()
 pygame.quit()
